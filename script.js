599bb7243dd4001c236507384b65a521af977953
unknown <AxoLath@gmail.com>
2021-09-10 13:38:01 +0700

    pindah ke localhost, ngasih nav ke setiap page

---
+++ script.js
@@ -0,0 +1,7 @@
+	const btnWa = document.getElementById('tombol-whatsapp');
+
+    const boxWhatsapp = document.getElementById('box-whatsapp');
+
+    btnWa.addEventListener('click', function () {
+    		boxWhatsapp.classList.toggle('wa-active');
+    	})
